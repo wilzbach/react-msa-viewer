c01f80d4adb3df857fd8ac6f7e7f239e43e8d33a
Sebastian Wilzbach <seb@wilzba.ch>
2018-08-03 15:59:43 +0200

    Init React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MSAViewer from './MSAViewer';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MSAViewer/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/MSAViewer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MSAViewer extends Component {
+  render() {
+    return (
+      <div>
+        MSAViewer
+      </div>
+    );
+  }
+}
+
+export default MSAViewer;
